216185d815c481ecf3dec9168d729c637e41a8d4
dhawaldj95 <dhawaldj18@gmail.com>
2019-01-29 14:19:21 +0530

    Added comments

--- routes/index.js
+++ routes/index.js
@@ -8,110 +8,19 @@ var assert = require('assert');
 var User = require('../lib/user');
 var Product = require('../lib/product');
 
-// var url = 'mongodb://localhost:27017';
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Form Validation', success: false, errors: req.session.errors });
-});
-
-// router.post('/insert', function(req, res, next) {
-//   var item = {
-//     title: req.body.title,
-//     content: req.body.content,
-//     author: req.body.author
-//   };
-//
-//   MongoClient.connect(url, { useNewUrlParser: true }, function(err, client) {
-//     assert.equal(null, err);
-//     var db = client.db('test1');
-//     db.collection('user-data').insertOne(item, function(err, result) {
-//       assert.equal(null, err);
-//       console.log("Item Inserted");
-//       client.close();
-//       res.json({message: 'success'});
-//     });
-//   })
-// });
-//
-// router.post('/retrieve', function (req, res, next) {
-//
-//   var resultArray = [];
-//   MongoClient.connect(url, { useNewUrlParser: true }, function (err, client)  {
-//     assert.equal(null, err);
-//     var db = client.db('test1');
-//     var cursor = db.collection('user-data').find();
-//     cursor.forEach(function(doc, err) {
-//       assert.equal(null, err);
-//       resultArray.push(doc);
-//     }, function() {
-//       client.close();
-//       res.json({output: resultArray});
-//     });
-//   });
-//
-// });
-//
-// router.post('/update', function (req, res, next) {
-//   var item = {
-//     title: req.body.title,
-//     content: req.body.content,
-//     author: req.body.author
-//   };
-//
-//   var id = req.body.id;
-//
-//   MongoClient.connect(url, { useNewUrlParser: true }, function(err, client) {
-//     assert.equal(null, err);
-//     var db = client.db('test1');
-//     db.collection('user-data').updateOne({"_id": objectId(id)}, {$set: item}, function(err, result) {
-//       assert.equal(null, err);
-//       console.log("Item Updated");
-//       client.close();
-//       res.json({message: 'success'});
-//     });
-//   })
-// });
-//
-//
-// router.post('/delete', function (req, res, next) {
-//   var id = req.body.id;
-//
-//   MongoClient.connect(url, { useNewUrlParser: true }, function(err, client) {
-//     assert.equal(null, err);
-//     var db = client.db('test1');
-//     db.collection('user-data').deleteOne({"_id": objectId(id)}, function(err, result) {
-//       assert.equal(null, err);
-//       console.log("Item Deleted");
-//       client.close();
-//       res.json({message: 'success'});
-//     });
-//   })
+// /* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Form Validation', success: false, errors: req.session.errors });
 // });
 
-// router.post('/submit', function(req, res, next) {
-//   req.check('email', 'Invalid Email Address').isEmail();
-//   req.check('password', 'Password is invalid').isLength({min:4});
-//
-//   var errors = req.validationErrors();
-//   if (errors) {
-//     req.session.errors = errors;
-//     var array = [];
-//     req.session.errors.forEach(function (element) {
-//         console.log(element.msg);
-//         array.push(element.msg);
-//     });
-//
-//     res.json({ message: array });
-//   }
-//   else {
-//     res.json({message: 'success'});
-//   }
-// });
-
-
+/**
+ * This api is used to register the user.
+ * Admin have to register directly an not through this api.
+ */
 router.post("/register", function(req, res) {
   var username = req.body.username;
+  // The password can be hashed and encrypted but to keep it simple here hashing is not implemented
   var password = req.body.password;
   var firstname = req.body.firstname;
   var lastname = req.body.lastname;
@@ -131,34 +40,31 @@ router.post("/register", function(req, res) {
       return res.status(500).send();
     }
     console.log(savedUser);
-    return res.status(200).send();
-
+    return res.status(200).json("Successfully Registered").send();
   });
 });
 
+/**
+ * Login api.
+ */
 router.post("/login", function(req, res) {
   var username = req.body.username;
   var password = req.body.password;
-
+  // Currently we are using the express session
+  // We can take a more advance approach by using jwt-token which provides advance options to
+  // store sessions and auto time out facilities
   User.findOne({username: username, password: password}, function (err, user) {
     if (err) {
       console.log(err);
-      res.status(500);
-      res.json(err);
-      return res.send();
+      return res.status(500).json(err).send("Access Denied");
     }
     if (!user) {
-      res.status(200);
-      res.json("No user found");
-      return res.send();
+      return res.status(200).json("No user found").send();
     }
     if (user) {
       console.log(user);
       req.session.user = user;
-
-      res.status(200);
-      res.json("user found");
-      return res.send();
+      return res.status(200).json("Logged in successfully").send();
     }
   });
 });
@@ -234,6 +140,11 @@ router.post("/product-update", function(req, res) {
   }
 });
 
+/**
+ * Delete api
+ * Admin can delete any of the products.
+ *
+ */
 router.post("/product-delete", function(req, res) {
   if (!req.session.user) {
     return res.status(403).send("Access Denied");
@@ -260,6 +171,10 @@ router.post("/product-delete", function(req, res) {
   }
 });
 
+/**
+ * This api can be accessed by admin only
+ * He/She can change the status of the code published/unpublished.
+ */
 router.post('/product-publish', function(req, res) {
   if (!req.session.user) {
     return res.status(403).send("Access Denied");
@@ -283,13 +198,19 @@ router.post('/product-publish', function(req, res) {
   }
 });
 
-
+/**
+ * This api can be accessed by all the uses (Anomyous users)
+ * This will display all the published content
+ */
 router.get('/retrieve-products', function (req, res) {
   Product.find({status: 'Published'}).then(function (doc) {
     return res.status(200).json(doc).send();
   });
 });
 
+/**
+ * Logout Api
+ */
 router.get('/logout', function(req, res, next) {
   if (req.session) {
     // delete session object
